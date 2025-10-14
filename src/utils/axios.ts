a4da864cb5d9ba13a8d9efbc3314b7dba63a5d95
Om Jadhav <om427880@gmail.com>
2025-10-14 21:55:16 +0530

    Update axios.ts

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -2,7 +2,7 @@ import axios, { AxiosInstance } from "axios";
 import Cookies from "js-cookie";
 
 const apiInstance: AxiosInstance = axios.create({
-  baseURL: "http://localhost:8000/api/v1/",
+  baseURL: "https://frl-server.onrender.com/api/v1/",
   timeout: 10000, // 10 seconds
   headers: {
     "Content-Type": "application/json",
